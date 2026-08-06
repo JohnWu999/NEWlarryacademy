d8ff3f2a570ee717655da0436f10079704eb8670
John Wu <JohnWu999@users.noreply.github.com>
2026-08-06 22:05:36 +0800

    Publish Larry learning and activities plan

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/myschedule",
+        destination: "/myschedule.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
